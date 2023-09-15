1b7b839d0255305deb1090b94444340785272673
Angelo Lustosa de Figueiredo Filho <01337098337@DIGITALCOLLEGE.LOCAL>
2023-09-15 01:05:08 -0300

    http

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const http = require('http')
+//const http = require('http')
+import http from "http";
 
-const HOST = 'localhost'
-const PORT  = 5000
+const HOST = "localhost";
+const PORT = 5000;
 
-const server = http.createServer((req, res) => {
-    res.end('Oi, estou funcionado com nodemon!')
+const server = createServer((req, res) => {
+  res.end("Oi, estou funcionado com nodemon!");
+});
+
+get('/teste', (req, res) => {
+    
 })
 
 server.listen(PORT, HOST, () => {
-    console.log(`Servidor Rodando: http://${HOST}:${PORT}/`);
-})
+  console.log(`Servidor Rodando: http://${HOST}:${PORT}/`);
+});
